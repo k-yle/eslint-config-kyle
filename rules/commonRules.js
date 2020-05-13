546adb802ec22c9e517d62d3ab198915f1c7a8f1
Kyle Hensel <k.y.l.e@outlook.co.nz>
2020-05-13 12:51:38 +1200

    spaced-comment and jsx-a11y/click-events-have-key-events

--- rules/commonRules.js
+++ rules/commonRules.js
@@ -5,6 +5,9 @@ module.exports = {
   'guard-for-in': 0,
   'import/prefer-default-export': 0,
 
+  // for ts <reference /> comments
+  'spaced-comment': [2, 'always', { markers: ['/'] }],
+
   // our added rules
   'prefer-object-spread': 1,
   'prefer-destructuring': 1,
@@ -15,4 +18,5 @@ module.exports = {
 
   // react
   'react/jsx-props-no-spreading': 0,
+  'jsx-a11y/click-events-have-key-events': 0,
 };
