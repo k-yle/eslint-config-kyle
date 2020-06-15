2aa188efe88e40c43169640c5dead1c81035c122
Kyle Hensel <k.y.l.e@outlook.co.nz>
2020-06-15 12:49:15 +1200

    bug fix

--- rules/commonRules.js
+++ rules/commonRules.js
@@ -2,6 +2,7 @@ module.exports = {
   // turn off crap
   'no-console': 0,
   'no-restricted-syntax': 0,
+  'no-await-in-loop': 0,
   'guard-for-in': 0,
   'import/prefer-default-export': 0,
 
--- rules/jest.js
+++ rules/jest.js
@@ -1,5 +1,9 @@
 module.exports = {
-  files: ['**/__tests__/*.test.js', '**/__tests__/*.test.ts'],
+  files: [
+    '**/__tests__/*.test.js',
+    '**/__tests__/*.test.ts',
+    '**/__tests__/*.test.tsx',
+  ],
   env: {
     jest: true,
   },
